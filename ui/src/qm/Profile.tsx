ef8741c0e88650d2e376c7716cf43e2f739321b3
pseudopirate <igor.vernichenko@gmail.com>
2024-03-23 17:13:00 +0200

    nav

---
+++ ui/src/qm/Profile.tsx
@@ -0,0 +1,13 @@
+import { Card } from 'antd';
+
+export default function Profile() {
+    return (
+        <div style={{ paddingTop: "20px"}}>
+          <Card style={{ width: '100%' }}>
+                <p>Card content</p>
+                <p>Card content</p>
+                <p>Card content</p>
+            </Card>
+        </div>
+    )
+}
--- ui/src/qm/index.tsx
+++ ui/src/qm/index.tsx
@@ -1,3 +1,34 @@
+import React, { useState } from 'react';
+import type { MenuProps } from 'antd';
+import { Menu } from 'antd';
+import Profile from './Profile';
+
+const items: MenuProps['items'] = [
+    {
+      label: 'Profile',
+      key: 'profile',
+    },
+    {
+      label: 'Quests',
+      key: 'quests',
+    },
+    {
+      label: 'Statistics',
+      key: 'statistics',
+    },
+  ];
+
 export function QM() {
-    return <div>Hello Quest Master</div>
+    const [current, setCurrent] = useState('profile');
+
+    const onClick: MenuProps['onClick'] = (e) => {
+      setCurrent(e.key);
+    };
+  
+    return (
+        <>
+        <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />
+        {current === 'profile' && <Profile/>}
+        </>
+    );
 }
